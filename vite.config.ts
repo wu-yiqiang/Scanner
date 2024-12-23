fcd377a1efa3e8800975dd29744b3ac67d352c64
wu_yiqiang <sutter.wu@itforce-tech.com>
2024-12-23 21:35:37 +0800

    qw

--- vite.config.ts
+++ vite.config.ts
@@ -10,20 +10,6 @@ const pathResolve = (dir: string): string => {
   return resolve(__dirname, '.', dir)
 }
 
-const getCache = ({ name, pattern }: any) => ({
-  urlPattern: pattern,
-  handler: 'CacheFirst' as const,
-  options: {
-    cacheName: name,
-    expiration: {
-      maxEntries: 500,
-      maxAgeSeconds: 60 * 60 * 24 * 365 * 2 // 2 years
-    },
-    cacheableResponse: {
-      statuses: [200]
-    }
-  }
-})
 export default defineConfig({
   // base: './',
   resolve: {

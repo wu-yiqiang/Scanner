b349ac88e3c9faeb7ef756dad64aa2462506d8ed
wu_yiqiang <sutter.wu@itforce-tech.com>
2026-03-26 10:32:43 +0800

    优化打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/scanner',
         icons: [
           {
             src: '/src/assets/images/logo-192.png',
